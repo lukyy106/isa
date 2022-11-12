bf94d59f1de26eb63dd3f30dbe51b2e787f68a2f
lukyy106 <lukasneupauer121@gmail.com>
2022-11-12 01:37:23 +0100

    glupy merlin

--- netflow.c
+++ netflow.c
@@ -1,4 +1,20 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pcap/pcap.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+//#include <netinet/icmp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "netflow.h"
 
 
